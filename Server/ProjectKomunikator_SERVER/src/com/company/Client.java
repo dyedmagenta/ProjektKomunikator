54f67fa56cabd36eb3056b93e430c0f71050c310
repca <repcak.kacper@hotmail.com>
2017-03-02 03:24:03 +0100

    Alpha Server

---
+++ Server/ProjectKomunikator_SERVER/src/com/company/Client.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by repcak on 28.02.2017.
+ */
+public class Client {
+
+
+}
---
+++ Server/ProjectKomunikator_SERVER/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ComServer server = new ComServer();
+                server.startServer();
+    }
+}
